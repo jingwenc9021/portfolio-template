bb749f9f99fa1c982fea48180300f7b7d52183b4
Jingwen <Jingwenc9021@hstat.org>
2021-09-24 14:01:01 +0000

    name

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Jingwen chen"
+let userName = "jingwenc9021";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
